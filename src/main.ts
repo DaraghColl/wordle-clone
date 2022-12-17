ef10ed35611969aacfb1f835f3c30451a6c62169
Daragh <daraghcoll-@hotmail.com>
2022-12-17 23:06:44 +0000

    feat: grid setup and check input correct

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,45 @@
 import './style.css';
-import typescriptLogo from './typescript.svg';
-import { setupCounter } from './counter';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+const wordleGrid = document.querySelector('.grid');
+const word = 'catch';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+const checkIfLetterCorrect = (item: HTMLInputElement, rowItemIndex: number) => {
+  if (item.value.toLowerCase() === word[rowItemIndex]) {
+    item.classList.add('letter-correct');
+  }
+};
+
+const rowItemInput = (event: InputEvent, rowItemIndex: number) => {
+  const target = event.target as HTMLInputElement;
+  target.disabled = true;
+
+  checkIfLetterCorrect(target, rowItemIndex);
+};
+
+const createRowItem = (rowItemIndex: number, row: number) => {
+  const rowItem = document.createElement('input');
+  rowItem.setAttribute('type', 'text');
+  rowItem.setAttribute('row', row.toString());
+  rowItem.setAttribute('row-item', rowItemIndex.toString());
+  rowItem.classList.add('grid__item');
+
+  // disable all inputs inputs except first input
+  if (rowItemIndex !== 0 && row === 0) {
+    rowItem.disabled = true;
+  }
+
+  rowItem.addEventListener('input', (event: any) => rowItemInput(event, rowItemIndex));
+
+  return rowItem;
+};
+
+const createRow = (row: number) => {
+  for (let i = 0; i <= 4; i++) {
+    const rowItem = createRowItem(i, row);
+    wordleGrid?.appendChild(rowItem);
+  }
+};
+
+for (let i = 0; i <= 3; i++) {
+  createRow(i);
+}
