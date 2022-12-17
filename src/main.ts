7e660df13c3122d836582d89d3f5486a4206b6c9
Daragh <daraghcoll-@hotmail.com>
2022-12-17 23:17:34 +0000

    feat: set matching columns to same value, color, and disabled

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,18 @@ const wordleGrid = document.querySelector('.grid');
 const word = 'catch';
 
 const checkInputAnswer = (item: HTMLInputElement, rowItemIndex: number) => {
+  // add correct class if letter is correct
+  // set matching columns to value and disabled
   if (item.value.toLowerCase() === word[rowItemIndex]) {
     item.classList.add('letter-correct');
+    const matchingRowItems = document.querySelectorAll<HTMLInputElement>(
+      `[row-item="${rowItemIndex}"]`,
+    );
+    matchingRowItems.forEach((rowItem) => {
+      rowItem.value = item.value;
+      rowItem.disabled = true;
+      rowItem.classList.add('letter-correct');
+    });
   }
 };
 
