694a0f86a745a739a3a5d3eaa8144aeb85795c8d
Daragh <daraghcoll-@hotmail.com>
2022-12-18 00:09:20 +0000

    feat: set game status

--- src/main.ts
+++ src/main.ts
@@ -2,33 +2,56 @@ import './style.css';
 
 const wordleGrid = document.querySelector('.grid');
 const word = 'catch';
+const gameState = {
+  gameOver: false,
+};
+
+const wordStatus: Record<string, boolean> = {};
+word.split('').forEach((key) => (wordStatus[key.toLowerCase()] = false));
+
+const checkGameStatus = (item: HTMLInputElement) => {
+  const allLettersCorrect = Object.values(wordStatus).every((value) => value === true);
+
+  if (allLettersCorrect) {
+    gameState.gameOver = true;
+  } else if (item.getAttribute('row') === '3' && item.getAttribute('row-item') === '4') {
+    gameState.gameOver = true;
+  }
+};
 
 const checkInputAnswer = (item: HTMLInputElement, rowItemIndex: number) => {
-  // add correct class if letter is correct
-  // set matching columns to value and disabled
-  if (item.value.toLowerCase() === word[rowItemIndex]) {
-    item.classList.add('letter-correct');
-    const matchingRowItems = document.querySelectorAll<HTMLInputElement>(
-      `[row-item="${rowItemIndex}"]`,
-    );
-    matchingRowItems.forEach((rowItem) => {
-      rowItem.value = item.value;
-      rowItem.disabled = true;
-      rowItem.classList.add('letter-correct');
-    });
+  if (!gameState.gameOver) {
+    // add correct class if letter is correct
+    // set matching columns to value and disabled
+    if (item.value.toLowerCase() === word[rowItemIndex]) {
+      wordStatus[item.value.toLowerCase()] = true;
+      item.classList.add('letter-correct');
+      const matchingRowItems = document.querySelectorAll<HTMLInputElement>(
+        `[row-item="${rowItemIndex}"]`,
+      );
+      matchingRowItems.forEach((rowItem) => {
+        rowItem.value = item.value;
+        rowItem.disabled = true;
+        rowItem.classList.add('letter-correct');
+      });
+    }
+
+    checkGameStatus(item);
   }
 };
 
 const findNextInput = (item: HTMLInputElement, rowItemIndex: number) => {
   const nextItem = item.nextElementSibling as HTMLInputElement;
 
-  // if letter already answered, check for next input again
-  if (nextItem.classList.contains('letter-correct')) {
-    findNextInput(nextItem, rowItemIndex);
-    return;
-  } else {
-    nextItem.disabled = false;
-    nextItem.focus();
+  if (!gameState.gameOver) {
+    // if letter already answered, check for next input again
+    if (nextItem.classList.contains('letter-correct')) {
+      findNextInput(nextItem, rowItemIndex);
+      return;
+    } else {
+      nextItem.disabled = false;
+      nextItem.focus();
+    }
   }
 };
 
